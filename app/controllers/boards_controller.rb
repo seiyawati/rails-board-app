486dbf41b76a2cf5ba16f6fdfa55115b201efaa4
Seiya Kawamoto <kid960805@gmail.com>
2020-12-28 16:15:50 +0900

    add: controller filter

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+    before_action :set_target_board, only: %i[show edit update destory]
+
     def index
         @boards = Board.all
     end
@@ -14,25 +16,20 @@ class BoardsController < ApplicationController
     end
 
     def show
-        @board = Board.find(params[:id])
     end
 
     def edit
-        @board = Board.find(params[:id])
     end
 
     def update
-        # インスタンス変数じゃないのはviewに渡す必要がないため
-        board = Board.find(params[:id])
-        board.update(board_params)
+        @board.update(board_params)
 
         # オブジェクトを指定すると/boards/:idに飛ぶ
-        redirect_to board
+        redirect_to @board
     end
 
     def destory
-        board = Board.find(params[:id])
-        board.delete
+        @board.delete
 
         redirect_to boards_path
     end
@@ -41,5 +38,9 @@ class BoardsController < ApplicationController
 
     def board_params
         params.require(:board).permit(:name, :title, :body)
+    end
+
+    def set_target_board
+        @board = Board.find(params[:id])
     end
 end
