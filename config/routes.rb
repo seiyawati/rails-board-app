2ff7fbe460eb234cfc56816c872175bddba4c43a
Seiya Kawamoto <kid960805@gmail.com>
2020-12-29 22:18:10 +0900

    create: user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'mypage', to: 'users#me'
-  get 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # rootはlocalhost:3000にきた時にルーティング
