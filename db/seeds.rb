bcacb237e424195e15656588707dcb2da9f5241f
Seiya Kawamoto <kid960805@gmail.com>
2020-12-29 17:48:54 +0900

    add: func tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,13 @@ if Rails.env == 'development'
     (1..50).each do |i|
         Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
     end
+
+    Tag.create([
+        { name: 'Ruby' },
+        { name: 'PHP' },
+        { name: 'Python' },
+        { name: 'JavaScript' },
+        { name: 'Fortran' },
+        { name: 'C++' },
+    ])
 end
