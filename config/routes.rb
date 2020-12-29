c180c6fa24750e2d55d19811a1f4ff2d7fad5202
Seiya Kawamoto <kid960805@gmail.com>
2020-12-29 21:06:00 +0900

    add: users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  get 'mypage', to: 'users#me'
+  get 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # rootはlocalhost:3000にきた時にルーティング
   # resources :boards, only: [:index, :new, :create, :show, :edit, :update] 
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
