0489457099d710768ac6869577c9a3efb10abc24
Seiya Kawamoto <kid960805@gmail.com>
2020-12-31 00:43:36 +0900

    add: users_controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,6 +16,51 @@ RSpec.describe UsersController, type: :controller do
             # assignsはcontrollerのインスタンス変数をテストする
             expect(assigns(:user)).to be_a_new User
         end
-        
+    end
+
+    describe "POST #create" do
+        context '正しいユーザー情報が返って来た場合' do
+            let(:params) do
+                { user: {
+                    name: 'user',
+                    password: 'password',
+                    password_confirmation: 'password'
+                    }
+                }
+            end
+
+            it "ユーザーが一人増えていること" do
+                expect { post :create, params: params }.to change(User, :count).by(1)  
+            end
+
+            it "マイページにリダイレクトされていること" do
+                expect(post :create, params: params).to  redirect_to(mypage_path)
+            end
+        end
+
+        context "パラメーターに正しいユーザー名、確認パスワードが含まれていない場合" do
+            #testケースでも変数を参照できるようになる
+            before do
+                post(:create, params: {
+                    user: {
+                        name: 'ユーザー１',
+                        password: 'password',
+                        password_confirmation: 'invalid_password'
+                    }
+                })
+            end
+
+            it "リファラーにリダイレクトされること" do
+                expect(response).to redirect_to(@referer)
+            end
+            
+            it "ユーザー名のエラーメッセージが含まれていること" do
+                expect(flash[:error_messages]).to include 'ユーザー名は小文字英数字で入力してください'
+            end
+
+            it "パスワードエラー確認のエラーメッセージが含まれていること" do
+                expect(flash[:error_messages]).to include 'パスワード（確認）とパスワードの入力が一致しません'  
+            end 
+        end
     end
 end
